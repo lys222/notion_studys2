75cca968ab226ffc72a75a6af4cd9274cd9b2ffb
222 <02seo222@gmail.com>
2025-02-24 17:24:28 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     role: "Cloud Security Engineer",
     bio: "Home Sweet Home",
     email: "02seo222@gmail.com",
-    linkedin: "Youngseo Lee",
+    linkedin: "youngseo-lee-a74378316",
     github: "lys222",
     instagram: "",
   },
